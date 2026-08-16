0ee46a485ba418d423fd8f4d84b893185eb205ac
priyanshu8346 <91590140+priyanshu8346@users.noreply.github.com>
2026-08-16 15:56:57 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        vector<vector<string>> ans;
+        vector<int> temp(26, 0);
+        string temp_s="";
+        unordered_map<string, vector<string>> freq;
+        for(int i = 0; i < n; i++){
+            for(char c: strs[i]){
+                temp[c - 'a']++;
+            }
+            for(int j = 0; j < 26; j++){
+                temp_s+=('#' + to_string(temp[j]));
+            }
+            freq[temp_s].push_back(strs[i]);
+            temp.assign(26, 0);
+            temp_s="";
+        }
+        for(auto &it: freq){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
