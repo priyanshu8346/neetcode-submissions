87edd2e17fea1f3f5d357f51fa3bd2ad45bf2ee8
priyanshu8346 <91590140+priyanshu8346@users.noreply.github.com>
2026-08-17 16:36:10 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int start = 0;
+        int end = nums.size() - 1;
+        while(start <= end){
+            if(nums[start] == val){
+                swap(nums[start], nums[end--]);
+                if(nums[start] != val)start++;
+            }
+            else{
+                start++;
+            }
+        }
+        return end+1;
+    }
+};
